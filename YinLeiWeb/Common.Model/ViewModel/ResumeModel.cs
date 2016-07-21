70bdd797165a6299c165f213756755879be63798
yinlei <963928480@qq.com>
2016-07-21 11:03:51 +0800

    first upload directory

---
+++ YinLeiWeb/Common.Model/ViewModel/ResumeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model.ViewModel
+{
+    public class ResumeModel : DbContext
+    {
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ YinLeiWeb/YinLeiWeb/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YinLeiWeb.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Photo()
+        {
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
